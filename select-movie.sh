b71ec254ab24a5c8fb5a6a24be64e2a4b9a90a37
BaGreal2 <artemonik17@gmail.com>
2025-07-03 23:48:29 +0200

    [select-movie] arx path

--- select-movie.sh
+++ select-movie.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-MOVIE_DIR="$HOME/Documents/Movies"
+MOVIE_DIR="$HOME/videos/movies"
 
 tmux new-window -n "watch-movie" "bash -c '
   DIR_NAME=\$(find \"$MOVIE_DIR\" -mindepth 1 -maxdepth 1 -type d | sed \"s|^$MOVIE_DIR/||\" | sort | fzf --no-multi --no-sort);
@@ -8,7 +8,7 @@ tmux new-window -n "watch-movie" "bash -c '
     DIR_PATH=\"$MOVIE_DIR/\$DIR_NAME\"
     FILE_NAME=\$(find \"\$DIR_PATH\" -mindepth 1 -maxdepth 1 -type f \\( -iname \"*.mp4\" -o -iname \"*.mkv\" -o -iname \"*.mov\" \\) | sed \"s|^\$DIR_PATH/||\" | sort | fzf --no-multi --no-sort);
     if [ -n \"\$FILE_NAME\" ]; then
-      iina \"\$DIR_PATH/\$FILE_NAME\" --mpv-fs
+      mpv \"\$DIR_PATH/\$FILE_NAME\" --fs
     fi
   fi
 '"
